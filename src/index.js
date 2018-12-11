18fc4a985c3588dd67b8c748e01948991ac8a63e
Phan Nghiem Hai <phannghiemhai@gmail.com>
2018-12-11 18:56:32 +0800

    Init Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class ReactPhannh extends React.Component {
+  render() {
+    return (
+      <div>This is react-phannh!</div>
+    );
+  }
+}
+export default ReactPhannh;
